3b3a8d3413e7a0bb887e05f6bbbadf2b65195b65
root <root@ip-172-31-19-80.ap-northeast-2.compute.internal>
2019-06-01 14:33:14 +0000

    Fix wsgi.py

--- conf/wsgi.py
+++ conf/wsgi.py
@@ -8,9 +8,18 @@ https://docs.djangoproject.com/en/2.1/howto/deployment/wsgi/
 """
 
 import os
+import sys
+
+BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
+sys.path.append(BASE_DIR)
+
+activate_this = '{0}/venv/bin/activate_this.py'.format(BASE_DIR)
+with open(activate_this) as f:
+    exec(f.read(), dict(__file__=activate_this))
 
 from django.core.wsgi import get_wsgi_application
 
-os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'conf.settings.development')
+os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'conf.settings.product')
 
 application = get_wsgi_application()
+
